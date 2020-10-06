ef5365b4e798a01fda40ec533c5f7593360ab50b
Luyao <luyao@LuyaotekiMBP.T-mobile.com>
2020-10-06 16:42:22 -0400

    shuffle cards

--- src/components/game/board.ts
+++ src/components/game/board.ts
@@ -3,10 +3,21 @@ import { Player } from "components/player/player";
 import { Mission } from "./mission";
 import { Trick } from "./trick";
 import { GameState } from "./types";
+import { PlayCard } from "components/card/play";
 
 export interface Board {
   players: Player[];
   mission: Mission;
   status: GameState;
   currentTrick: Trick;
+}
+
+export const shufflePlayCards = (deck:Array<PlayCard>):void => {
+  for (let index = 0; index < deck.length; index++) {
+    let k : number = Math.floor(Math.random() * (index+1)); //generate random number between 0 and index, inclusive
+    let temp : PlayCard = deck[k];
+    deck[k] = deck[index];
+    deck[index] = temp;
+
+  }
 }
